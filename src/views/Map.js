110e1c51d1edd146df1073c96ff7701ed6e802df
shadin24 <shadinkvr@gmail.com>
2024-03-15 21:24:45 +0530

    with error

--- src/views/Map.js
+++ src/views/Map.js
@@ -54,3 +54,27 @@ const MapContainer = ({ apiKey }) => {
 };
 
 export default MapContainer;
+const Map = () => {
+  return (
+    <div className="content">
+      <Row>
+        <Col md="12">
+          <Card className="card-plain">
+            <CardHeader>Google Maps</CardHeader>
+            <CardBody>
+              <div
+                id="map"
+                className="map"
+                style={{ position: "relative", overflow: "hidden" }}
+              >
+                <MapContainer />
+              </div>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Map;
